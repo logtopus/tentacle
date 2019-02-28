b87dbeabd9f9518be9552bbaa56d43d4f086f0be
niveau0 <plingplong@t-online.de>
2019-02-28 22:08:02 +0100

    fix CI

--- tests/server_test.rs
+++ tests/server_test.rs
@@ -11,51 +11,70 @@ mod support;
 
 #[test]
 fn itest_health_api() {
-    support::run_test(setup, || {
-        fn request() -> impl futures::Future<Item=(), Error=support::TestError> {
+    support::run_test(
+        setup,
+        || {
+            fn request() -> impl futures::Future<Item = (), Error = support::TestError> {
+                let health_api_request =
+                    actix_web::client::ClientRequest::get("http://localhost:18080/api/v1/health")
+                        .header("User-Agent", "Actix-web")
+                        .timeout(std::time::Duration::from_millis(1000))
+                        .finish()
+                        .unwrap();
 
-            let health_api_request = actix_web::client::ClientRequest::get("http://localhost:18080/api/v1/health")
-                .header("User-Agent", "Actix-web")
-                .timeout(std::time::Duration::from_millis(1000))
-                .finish().unwrap();
+                health_api_request
+                    .send()
+                    .map_err(|_| support::TestError::Retry)
+                    .map(|r| {
+                        assert!(
+                            r.status() == http::StatusCode::OK,
+                            format!("Query failed with error code {}", r.status())
+                        );
+                        r.body()
+                    })
+                    .map(
+                        |body| {
+                            body.map_err(|e| {
+                                assert!(false, e);
+                                support::TestError::Fail
+                            })
+                            .map(
+                                |bytes| {
+                                    std::str::from_utf8(&bytes)
+                                        .map_err(|e| {
+                                            assert!(false, e);
+                                            support::TestError::Fail
+                                        })
+                                        .map(|s| {
+                                            assert!(s == "");
+                                            Ok(())
+                                        })
+                                        .and_then(|r| r)
+                                }, // flatten result
+                            )
+                            .and_then(|r| r)
+                        }, // flatten result
+                    )
+                    .and_then(|r| r) // flatten result
+            }
 
-            health_api_request.send()
-                .map_err(|_| {
-                    support::TestError::Retry
-                })
-                .map(|r| {
-                    assert!(r.status() == http::StatusCode::OK, format!("Query failed with error code {}", r.status()));
-                    r.body()
-                })
-                .map(|body|
-                    body
-                        .map_err(|e| {
-                            assert!(false, e);
-                            support::TestError::Fail
-                        })
-                        .map(|bytes|
-                            std::str::from_utf8(&bytes)
-                                .map_err(|e| {
-                                    assert!(false, e);
-                                    support::TestError::Fail
-                                })
-                                .map(|s| {
-                                    assert!(s == "");
-                                    Ok(())
-                                })
-                                .and_then(|r| r) // flatten result
-                        )
-                        .and_then(|r| r) // flatten result
-                )
-                .and_then(|r| r) // flatten result
-        }
-
-        support::run_with_retries(&request, 10, "Failed to query health api")
-    }, teardown)
+            support::run_with_retries(&request, 10, "Failed to query health api")
+        },
+        teardown,
+    )
 }
 
 fn setup() -> std::process::Child {
-    let server = std::process::Command::new("target/debug/tentacle").arg("--config=tests/integrationtests.yml").spawn().expect("Failed to run server");
+    use std::fs;
+
+    let exe = fs::metadata("target/release/tentacle")
+        .map(|_| "target/release/tentacle")
+        .unwrap_or("target/debug/tentacle");
+
+    let server = std::process::Command::new(exe)
+        .arg("--config=tests/integrationtests.yml")
+        .spawn()
+        .expect("Failed to run server");
     server
 }
 
